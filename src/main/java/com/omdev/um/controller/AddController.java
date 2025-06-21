8926562f1a3610076fff985ae2a98e0935c2ef30
Ultron-Fryday <omraob1999@gmail.com>
2025-06-21 22:50:28 +0530

    Initial Commit, AddContorller added , also H2 dependancy added for test purpose

---
+++ src/main/java/com/omdev/um/controller/AddController.java
@@ -0,0 +1,15 @@
+package com.omdev.um.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AddController {
+
+	@GetMapping("/")
+	public String display() {
+		return "index";
+	}
+	
+	
+}
